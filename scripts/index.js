fa491e3b282d4e7d19f809e7ef141d7b6c62a7f8
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-15 21:54:26 -0800

    modify add button functionality to create div with new task and attach to to-do section

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
   $("#addTask").click(function() { // click event handler
     if ($(".textBox").val() != "") {
-      alert("New Task: " + $(".textBox").val());
-      $(".textBox").val(""); // clear out the text box
+      const taskElement = $("<div> </div>");
+      taskElement.addClass("task");
+      taskElement.text($(".textBox").val());
+
+      const deleteBtnElement = $("<button id='delete'> </button>");
+      deleteBtnElement.addClass("fas fa-trash-alt");
+
+      const doneBtnElement = $("<button id='done'> </button>");
+      doneBtnElement.addClass("fas fa-check");
+
+      taskElement.append(deleteBtnElement, doneBtnElement);
+
+      $(".notCompleted").append(taskElement);
+
+      $(".textBox").val("");
+
     } else {
       alert("Error: Please enter a task before clicking the 'Add' button.");
     }
