e7c1963c474fb1e49f5cd997e4eb06436db3f4fd
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-01-16 13:03:03 -0800

    implement functionality for server button to make a geet request to mock api for to-do tasks

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#import").click(function() {
+    jQuery.get("https://5f28559af5d27e001612eebf.mockapi.io/tasks/",
+      function(data, textStatus) {
+        const todoTasks = data.tasks;
 
-  $("div").on("click", "#done", function() {// use on method because button element is dynamically created so $("#done").click() won't work
+        for (let i = 0; i < todoTasks.length; i++) { // loop over list and add each task
+          const taskElement = $("<div> </div>");
+          taskElement.addClass("task");
+          taskElement.text(todoTasks[i]);
+
+          const deleteBtnElement = $("<button id='delete'> </button>");
+          deleteBtnElement.addClass("fas fa-trash-alt");
+
+          const doneBtnElement = $("<button id='done'> </button>");
+          doneBtnElement.addClass("fas fa-check");
+
+          taskElement.append(deleteBtnElement, doneBtnElement);
+          $(".notCompleted").append(taskElement);
+        }
+      },
+      "json");
+  });
+
+  $("div").on("click", "#done", function() { // use on method because button element is dynamically created so $("#done").click() won't work
     const taskElement = $(this).parent(); // this extracts the whole div
 
     taskElement.fadeOut(1000, function() {
